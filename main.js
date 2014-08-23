0fefd11669e25e92099269cfe170bd1a841aeee4
Amber Moss <amberdmoss@gmail.com>
2014-08-23 15:12:31 -0600

    swipe test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var chefApp = angular.module('chefApp', ['firebase']);
+var chefApp = angular.module('chefApp', ['$scope','firebase', 'ngTouch']);
 
 
 // chefApp.controller('', ['$scope', function($scope){
@@ -21,3 +21,35 @@ chefApp.controller('MainCtrl', ['$scope', function($scope){
 
   	$scope.info="ambs data"
 });
+
+
+$scope.recipes = [{
+        "name": "Chicken Tacos",
+        "pic_square": "https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-ash3\/t5\/174266_842841_1023237973_q.jpg",
+        "current_location": {
+            "city": "New York",
+            "state": "New York",
+            "country": "United States",
+            "zip": "",
+            "latitude": "40.7167",
+            "longitude": "-74",
+            "id": "108424279189115",
+            "name": "New York, New York"
+        },
+
+        {
+        "name": "Bacon Wrapped Steak",
+        "pic_square": "https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-ash2\/t5\/1117694_1614542_108355616_q.jpg",
+        "current_location": {
+            "city": "Houston",
+            "state": "Texas",
+            "country": "United States",
+            "zip": "",
+            "latitude": "29.7629",
+            "longitude": "-95.3832",
+            "id": "115963528414384",
+            "name": "Houston, Texas"
+        },
+  
+
+}]);
